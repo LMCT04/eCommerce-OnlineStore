9dd878b3ce5e44cefa78bf13aebd197f2ba33d3f
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-02-04 12:58:02 -0300

    ConnectDB

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const { conn } = require('./src/db.js')
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT,async () => {
-    await conn.sync({ force: true });    // CUANDO ESTOY EN DESAROLLO, LA DB SE ACTUALIZA BORRANDO Y CREANDO
-    //await conn.sync({ alter: true });     CUANDO ESTOY EN PRODUCCION, LA DB SE ACTUALIZA SIN PERDER LOS DATOS, SINO ADAPTANDO
+    // await conn.sync({ force: true });    // CUANDO ESTOY EN DESAROLLO, LA DB SE ACTUALIZA BORRANDO Y CREANDO
+    await conn.sync({ alter: true });    // CUANDO ESTOY EN PRODUCCION, LA DB SE ACTUALIZA SIN PERDER LOS DATOS, SINO ADAPTANDO
     console.log(`server listening on port http://localhost:${PORT}`);
 })
