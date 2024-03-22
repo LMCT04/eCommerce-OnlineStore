dbe672671e1ceb60012ec169b5e9813f422c05c4
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-03-22 10:02:08 -0300

    feature(client): Redux Persist, changes button, dialog, userCard

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,22 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistVar = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <PersistGate persistor={persistVar}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
